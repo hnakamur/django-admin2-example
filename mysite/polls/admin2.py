814827d0f764e34248ddca4060c7baeae1c51d14
Hiroaki Nakamura <hnakamur@gmail.com>
2015-08-21 13:30:58 +0900

    Use the traditional admin form for django.contrib.auth.models.User
    However I got the following error when accessing http://localhost:8000/admin2/auth/user/create/
    ImproperlyConfigured at /admin2/auth/user/create/
    <class 'django.contrib.auth.models.User'> is already registered in django-admin2

--- mysite/polls/admin2.py
+++ mysite/polls/admin2.py
@@ -1,7 +1,17 @@
+from django.contrib.auth.forms import UserCreationForm, UserChangeForm
+from django.contrib.auth.models import User
+
 from .models import Question, Choice
 
 import djadmin2
 
+class UserAdmin2(djadmin2.ModelAdmin2):
+    # Replicates the traditional admin for django.contrib.auth.models.User
+    create_form_class = UserCreationForm
+    update_form_class = UserChangeForm
+
+
 #  Register each model with the admin
 djadmin2.default.register(Question)
 djadmin2.default.register(Choice)
+djadmin2.default.register(User, UserAdmin2)
